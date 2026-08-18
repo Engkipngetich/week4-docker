64159728f1c44cfe792730d67d90b47a9d529298
kipngetich <enginkipngetichkoskei@gmail.com>
2026-08-18 20:55:14 +0300

    Add Dockerfile Node.js application

---
+++ node-app/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(`
+        <h1>Hello from Docker!</h1>
+        <p>Node.js application running inside a Docker container.</p>
+    `);
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
