6db102da4414dccdb7ac661dba9e71a5816af370
kipngetich <enginkipngetichkoskei@gmail.com>
2026-09-22 00:22:00 +0300

    Update application for rolling deployment

--- node-app/server.js
+++ node-app/server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end(`
         <h1>Hello from Docker!</h1>
-        <p>Node.js application running inside a Docker container.</p>
+        <p>Node.js application running inside a Docker container new version.</p>
     `);
 });
 
